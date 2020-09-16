fe7cbbb36d6cd226aedebd5604a81c8d3d4b3b62
Anders Schau Knatten <anders@knatten.org>
2020-09-16 15:47:52 +0200

    Don't qualify names all over

--- TestOstrichLib/test_parser.cpp
+++ TestOstrichLib/test_parser.cpp
@@ -6,7 +6,7 @@ import Ostrich;
 
 using Catch::Matchers::Contains;
 using Catch::Matchers::Equals;
-using namespace ostrich; //TODO remove qualifiers in the source code now that we have this here
+using namespace ostrich;
 using enum RegisterName;
 
 namespace
@@ -14,7 +14,7 @@ namespace
     // TODO test concepts here?
     // TODO now we can use equality operator instead
     template <typename InstructionType>
-    InstructionType checkInstruction(const ostrich::Instruction &parsedInstruction)
+    InstructionType checkInstruction(const Instruction &parsedInstruction)
     {
         INFO("Checking " << std::visit([](const auto i) { return i.toString(); }, parsedInstruction))
         REQUIRE(std::holds_alternative<InstructionType>(parsedInstruction));
@@ -25,48 +25,47 @@ namespace
 
 TEST_CASE("Inc")
 {
-    const auto inc = checkInstruction<ostrich::Inc>(ostrich::parseInstruction("inc rax"));
-    REQUIRE(inc.registerName == ostrich::RegisterName::rax);
+    const auto inc = checkInstruction<Inc>(parseInstruction("inc rax"));
+    REQUIRE(inc.registerName == rax);
 }
 
 TEST_CASE("Dec")
 {
-    const auto dec = checkInstruction<ostrich::Dec>(ostrich::parseInstruction("dec rbx"));
-    REQUIRE(dec.registerName == ostrich::RegisterName::rbx);
+    const auto dec = checkInstruction<Dec>(parseInstruction("dec rbx"));
+    REQUIRE(dec.registerName == rbx);
 }
 
 TEST_CASE("Push")
 {
-    const auto push = checkInstruction<ostrich::Push>(ostrich::parseInstruction("push rax"));
-    REQUIRE(push.registerName == ostrich::RegisterName::rax);
+    const auto push = checkInstruction<Push>(parseInstruction("push rax"));
+    REQUIRE(push.registerName == rax);
 }
 
 TEST_CASE("Pop")
 {
-    const auto pop = checkInstruction<ostrich::Pop>(ostrich::parseInstruction("pop rbx"));
-    REQUIRE(pop.registerName == ostrich::RegisterName::rbx);
+    const auto pop = checkInstruction<Pop>(parseInstruction("pop rbx"));
+    REQUIRE(pop.registerName == rbx);
 }
 
 TEST_CASE("Add")
 {
-    const auto add = checkInstruction<ostrich::Add>(ostrich::parseInstruction("add rax rbx"));
-    REQUIRE(add.destination == ostrich::RegisterName::rax);
-    REQUIRE(add.source == ostrich::RegisterName::rbx);
+    const auto add = checkInstruction<Add>(parseInstruction("add rax rbx"));
+    REQUIRE(add.destination == rax);
+    REQUIRE(add.source == rbx);
 }
 
 TEST_CASE("Mov")
 {
-    const auto mov = checkInstruction<ostrich::Mov>(ostrich::parseInstruction("mov rbx ff"));
-    REQUIRE(mov.destination == ostrich::RegisterName::rbx);
+    const auto mov = checkInstruction<Mov>(parseInstruction("mov rbx ff"));
+    REQUIRE(mov.destination == rbx);
     REQUIRE(mov.value == 0xff);
 }
 
 TEST_CASE("Unknown instructions or empty source lines")
 {
-    CHECK_THROWS_WITH(ostrich::parseInstruction("lol wat"), Contains("Failed to parse 'lol wat'"));
-    CHECK_THROWS_WITH(ostrich::parseInstruction("wat"), Contains("Failed to parse 'wat'"));
-    CHECK_THROWS_WITH(ostrich::parseInstruction(""),
-                      Contains("Failed to parse empty source line!"));
+    CHECK_THROWS_WITH(parseInstruction("lol wat"), Contains("Failed to parse 'lol wat'"));
+    CHECK_THROWS_WITH(parseInstruction("wat"), Contains("Failed to parse 'wat'"));
+    CHECK_THROWS_WITH(parseInstruction(""), Contains("Failed to parse empty source line!"));
 }
 
 TEST_CASE("Parsing full source")
@@ -74,5 +73,6 @@ TEST_CASE("Parsing full source")
     CHECK(parse("").empty());
     CHECK(Instruction{ Inc{ rax } } == parse("inc rax").at(0));
     CHECK(Instruction{ Mov{ rbx, 2 } } == parse("mov rbx 2").at(0));
-    CHECK_THAT(parse("inc rax\ndec rbx"), Equals(std::vector{ Instruction{ Inc{ rax } }, Instruction{ Dec{ rbx } } }));
+    CHECK_THAT(parse("inc rax\ndec rbx"),
+               Equals(std::vector{ Instruction{ Inc{ rax } }, Instruction{ Dec{ rbx } } }));
 }
