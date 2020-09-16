2188929d6405c826c2a44cf149a4aa348d16d0fa
Anders Schau Knatten <anders@knatten.org>
2020-09-16 15:21:06 +0200

    Use parser in demo
    Nicer to have actual source code than manually coded instructions

--- Demo/demo.cpp
+++ Demo/demo.cpp
@@ -11,13 +11,14 @@ void print(const std::string_view &s)
 
 int main()
 {
-    // TODO parse and popuplate in constructor
-    ostrich::Vm vm{ ostrich::Source{
-    ostrich::Mov{ ostrich::RegisterName::rax, 0xfedcba9876543210 }, ostrich::Push{ ostrich::RegisterName::rax },
-    ostrich::Pop{ ostrich::RegisterName::rbx }, ostrich::Mov{ ostrich::RegisterName::rbx, 0x111 },
-    ostrich::Add{ .destination = ostrich::RegisterName::rax, .source = ostrich::RegisterName::rbx },
-    ostrich::Push{ ostrich::RegisterName::rax }, ostrich::Push{ ostrich::RegisterName::rax },
-    ostrich::Dec{ ostrich::RegisterName::rbx } } };
+    const char *source = "mov rax 0xfedcba9876543210\n"
+                         "push rax\n"
+                         "pop rbx\n"
+                         "mov rbx 0x111\n"
+                         "add rax rbx\n"
+                         "push rax\n"
+                         "dec rbx\n";
+    ostrich::Vm vm{ ostrich::parse(source) };
     ostrich::UI ui(120, 30, vm);
     ui.mainLoop();
     std::cin.get();
