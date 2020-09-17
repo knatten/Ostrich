646b416d859ebcfc0af25f53c9dff311e45f6be6
Anders Schau Knatten <anders@knatten.org>
2020-09-17 08:41:32 +0200

    Add quit command

--- OstrichLib/UI.cpp
+++ OstrichLib/UI.cpp
@@ -69,6 +69,10 @@ namespace ostrich
             {
                 m_vm.step();
             }
+            if(previousCommand == "q" || previousCommand == "quit")
+            {
+                return;
+            }
         }
         render();
     }
